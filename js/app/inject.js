afb81cbeccebf1448253555601ffe664bf56e4d0
Sergei Hanka <sergeihanka@users.noreply.github.com>
2016-05-17 15:02:51 -0500

    added variable for inputID

--- js/app/inject.js
+++ js/app/inject.js
@@ -1,16 +1,11 @@
-﻿// my.luther form id's 
-var subjectBox = "#LIST_VAR1_";
-var courseNumberBox = "#LIST_VAR3_";
-var sectionNumberBox = "#LIST_VAR4_";
-
+﻿var titleID = "fields[title]";
 
 // Listen for message to register schedule
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	console.log("register_schedule...");
 	if (request.message === "test1") {
 		console.log("test1...");
-		console.log(document.getElementById("fields[title]"));
-		document.getElementById("fields[title]").value = "TEST!";
-		console.log(document.getElementById("fields[title]"));
+		console.log(document.getElementById(titleID));
+		document.getElementById(titleID).value = "TEST!";
 	}
 });
