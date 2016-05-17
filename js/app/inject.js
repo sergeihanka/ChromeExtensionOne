4cf0a771f448313de09999087a59e079cb2c5e06
Sergei Hanka <sergeihanka@users.noreply.github.com>
2016-05-17 15:11:57 -0500

    fixing typos and missed bugs

--- js/app/inject.js
+++ js/app/inject.js
@@ -1,11 +1,20 @@
 ﻿var titleID = "fields[title]";
+var impactID = "s2id_autogen1";
+var statusID = "fields[title]";
+var categoryID = "fields[title]";
+var firstContactID = "fields[title]";
+var ownerID = "fields[title]";
+var approverID = "fields[title]";
+var resolvedID = "fields[title]";
+var commentID = "fields[title]";
 
-// Listen for message to register schedule
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log("register_schedule...");
-	if (request.message === "test1") {
+	console.log("filling template...");
+	if (request.message === "fill_in_template") {
 		console.log("test1...");
-		console.log(document.getElementById(titleID));
-		document.getElementById(titleID).value = "TEST!";
+		console.log(document.getElementById("titleID"));
+		document.getElementById(titleID).value = "Test!";
+		document.getElementById(impactID).value = "Other";
+
 	}
 });
--- js/app/popup.js
+++ js/app/popup.js
@@ -8,7 +8,7 @@
 		$scope.$apply();
 
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {"message": "test1"});
+			chrome.tabs.sendMessage(tabs[0].id, {"message": "fill_in_template"});
 		});
 	}, false);
 });
